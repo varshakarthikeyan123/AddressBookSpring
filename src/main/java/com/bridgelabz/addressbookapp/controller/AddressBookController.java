5723e95bc56a3814a8c71ff4d158fbc742513755
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-03-25 11:57:32 +0530

    [Varsha] : Added : UC2 : Address Book CRUD APIs

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,13 +1,38 @@
 package com.bridgelabz.addressbookapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/addressbook")
 public class AddressBookController {
 
-    @GetMapping("/addressbook")
+    // GET
+    @GetMapping("")
     public String getMessage() {
-        return "Welcome to Address Book App";
+        return "Get Call Successful";
+    }
+
+    // GET by ID
+    @GetMapping("/{id}")
+    public String getById(@PathVariable int id) {
+        return "Get Call for ID: " + id;
+    }
+
+    // POST
+    @PostMapping("")
+    public String create(@RequestParam String name) {
+        return "Created entry for: " + name;
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public String update(@PathVariable int id, @RequestParam String name) {
+        return "Updated ID " + id + " with name: " + name;
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id) {
+        return "Deleted entry with ID: " + id;
     }
 }
